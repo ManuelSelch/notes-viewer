7ffe59c44e82dadd557e93995a4b12e29c51f870
Manuel Selch <manuel@selch.de>
2026-05-02 11:34:18 +0200

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Manuel Selch on 02.05.26.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
